4c3b6e96aecaacd51dcf6bd51d6e032427a9ce62
tqh236 <1924283924@qq.com>
2021-03-22 14:09:48 +0800

    '第一次提交代码'

---
+++ src/main/java/com/example/myproject/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.myproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping(value = "/hello")
+    public String hello(){
+return "hello";
+    }
+}
---
+++ src/test/java/com/example/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.myproject;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
